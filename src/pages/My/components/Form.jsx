5fd7fe3c357d22bede70a00b5df0912b30bd5e14
Zakee ul Hassan <zakki5shah@gmail.com>
2025-01-21 01:47:34 -0300

    profile page updated

--- src/pages/My/components/Form.jsx
+++ src/pages/My/components/Form.jsx
@@ -3,7 +3,7 @@ import { IoCopy } from "react-icons/io5";
 
 const Form = () => {
   return (
-    <form className="text-white mb-12 text-lg flex outline-none border-none gap-1 flex-col">
+    <form className="text-white mb-20 text-lg flex outline-none border-none gap-1 flex-col">
       <div>
         <label htmlFor='name'>Mecca Crew Name</label>
         <div className="flex items-center justify-between text-mypurple-600 bg-white px-2 py-2 rounded-md">
