a49ec5a43832b5265eace0fbcb2b5a0a6553efc5
Zakee ul Hassan <zakki5shah@gmail.com>
2025-01-25 01:37:23 -0300

    removed circe

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,30 +4,161 @@ import Character from './components/Character'
 import Icons from './components/Icons'
 import './Main.css'
 
-const Main = () => {
-  const [tapCount, setTapCount] = useState(0);
+import logo from '../../assets/main/logo.png'
 
-  const handleTap = () => {
+import chara from '../../assets/gif/3d-idle.gif'
+import chara_fly from '../../assets/gif/chara-fly-bronze.gif'
+import chara_land from '../../assets/gif/chara-landing.gif'
+import chara_panic from '../../assets/gif/chara-fall-panic.gif'
+import chara_crash from '../../assets/gif/chara-crash.gif'
+import chara_dance from '../../assets/gif/chara-dance.gif'
+
+const Main = () => {
+  const [tapCount, setTapCount] = useState(0)
+  const [logoVisible, setLogoVisible] = useState(false)
+  const [logoStyle, setLogoStyle] = useState({})
+  const [characterState, setCharacterState] = useState('idle')
+  const [inactivityTimer, setInactivityTimer] = useState(null)
+
+  const handleTap = (event) => {
+    if (tapCount >= 30) return // Prevent taps after 30
+
+    const xPos = event.clientX;
+    const yPos = event.clientY;
+    // Increment tap count
     setTapCount(prev => {
-      if (prev >= 100) return 100 // Stop increasing when it reaches 100%
-      return prev + 100 // Increment gauge height by 10% per tap
+      const newTapCount = prev + 1
+
+      // Character transitions based on taps
+      if (newTapCount === 30) {
+        setCharacterState('land')
+        setTimeout(() => setCharacterState('dance'), 600)
+      } else {
+        setCharacterState('fly')
+      }
+
+      return newTapCount
     })
+
+    // Animate and show logo
+    setLogoVisible(true)
+    setLogoStyle({
+      opacity: 1,
+      transform: 'translateY(100px) translateX(100px)',
+      left: `${xPos - 150}px`, // Offset for better positioning
+      top: `${yPos - 150}px`, // Offset for better positioning
+
+      position: 'absolute',
+      transition: 'transform 1s ease, opacity 1s ease',
+      zIndex: 9999
+    })
+
+    // Reset logo visibility after animation
+    setTimeout(() => {
+      setLogoStyle({
+        opacity: 0,
+        transform: 'translateY(0) translateX(0)',
+        left: `${xPos -150}px`, // Keep it at tap position
+        top: `${yPos -150}px`, // Keep it at tap position
+        position: 'absolute',
+        zIndex: 9999
+      })
+      setTimeout(() => setLogoVisible(false), 500)
+    }, 500)
+
+    // Reset inactivity timer
+    if (inactivityTimer) clearTimeout(inactivityTimer)
+
+    const timer = setTimeout(() => {
+      if (tapCount < 29) {
+        setCharacterState('crash')
+        setTimeout(() => setCharacterState('panic'), 1000)
+      }
+    }, 2000) // 2 seconds of inactivity
+
+    setInactivityTimer(timer)
+  }
+
+  useEffect(() => {
+    return () => {
+      if (inactivityTimer) clearTimeout(inactivityTimer) // Clean up timer
+    }
+  }, [inactivityTimer])
+
+  const getCharacterGif = () => {
+    switch (characterState) {
+      case 'fly':
+        return chara_fly
+      case 'land':
+        return chara_land
+      case 'panic':
+        return chara_panic
+      case 'crash':
+        return chara_crash
+      case 'dance':
+        return chara_dance
+      default:
+        return chara
+    }
+  }
+
+  const characterStyles = {
+    width: characterState === 'panic' ? '75%' : '100%',
+    transform: characterState === 'panic' ? 'translate(30px, 30px)' : 'none',
+    transition: 'transform 0.5s ease, width 0.5s ease'
+  }
+
+  const getBackgroundStyle = () => {
+    return {
+      backgroundPosition:
+        characterState === 'panic' ? '0px -1680px' : `${tapCount * -13}px 0px`,
+      transition:
+        characterState === 'panic'
+          ? 'background-position 100s ease-out'
+          : 'background-position 1s ease-out'
+    }
   }
 
   return (
     <div className='main-container'>
-      <div className='bg-main'>
+      <div
+        className={`bg-planet ${
+          tapCount < 30 ? 'cursor-pointer' : 'cursor-not-allowed'
+        }`}
+        style={getBackgroundStyle()}
+      >
         <Icons />
         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-          <Character tapCount={tapCount} setTapCount={setTapCount} />
+          <Character
+            handleTap={handleTap}
+            tapCount={tapCount}
+            setTapCount={setTapCount}
+            getCharacterGif={getCharacterGif}
+            characterStyles={characterStyles}
+          />
+
+          {/* Animated Logo */}
+          {logoVisible && (
+            <img
+              src={logo}
+              alt=''
+              className='w-10 absolute'
+              style={{
+                ...logoStyle,
+                transition: 'transform 1s ease, opacity 1s ease'
+              }}
+            />
+          )}
         </div>
+
+        {/* Gauge */}
         <div
           className='h-96 w-6 flex flex-col justify-end bg-gray-200 absolute top-1/2 right-1 rounded-2xl transform -translate-x-1/2 -translate-y-1/2 overflow-hidden cursor-pointer'
-          onClick={handleTap} // Increment gauge height on tap
+          onClick={handleTap}
         >
           <div
             className='bg-mypurple-600 rounded-b-2xl transition-all duration-300'
-            style={{ height: `${tapCount*3.333}%` }} // Dynamically set the gauge height
+            style={{ height: `${tapCount * 3.333}%` }}
           ></div>
         </div>
         <Menu />
--- src/pages/Main/components/Character.jsx
+++ src/pages/Main/components/Character.jsx
@@ -7,105 +7,18 @@ import chara_crash from '../../../assets/gif/chara-crash.gif'
 import chara_dance from '../../../assets/gif/chara-dance.gif'
 import '../Main.css'
 
-const Character = ({ tapCount, setTapCount }) => {
-  const [characterState, setCharacterState] = useState('idle')
-  const [inactivityTimer, setInactivityTimer] = useState(null)
-  const tapCountRef = useRef(tapCount)
-
-  useEffect(() => {
-    tapCountRef.current = tapCount
-  }, [tapCount])
-
-  const handleTap = () => {
-    if (tapCount >= 30) return
-
-    setTapCount(prev => {
-      const newTapCount = prev + 1
-
-      if (newTapCount === 30) {
-        setCharacterState('land')
-        setTimeout(() => setCharacterState('dance'), 600)
-      } else {
-        setCharacterState('fly')
-      }
-
-      return newTapCount
-    })
-
-    if (inactivityTimer) clearTimeout(inactivityTimer)
-
-    const timer = setTimeout(() => {
-      if (tapCountRef.current < 30) {
-        setCharacterState('crash')
-        setTimeout(() => setCharacterState('panic'), 1000)
-      }
-    }, 2000)
-
-    setInactivityTimer(timer)
-  }
-
-  useEffect(() => {
-    return () => {
-      if (inactivityTimer) clearTimeout(inactivityTimer)
-    }
-  }, [inactivityTimer])
-
-  const getCharacterGif = () => {
-    switch (characterState) {
-      case 'fly':
-        return chara_fly
-      case 'land':
-        return chara_land
-      case 'panic':
-        return chara_panic
-      case 'crash':
-        return chara_crash
-      case 'dance':
-        return chara_dance
-      default:
-        return chara
-    }
-  }
-
-  const characterStyles = {
-    width: characterState === 'panic' ? '50%' : '100%',
-    transform:
-      characterState === 'panic' ? 'translate(90px, 30px)' : 'translate(0, 0)',
-    transition: 'transform infinite ease, width infinite ease'
-  }
-
-  const getBackgroundStyle = () => {
-    if (characterState === 'panic') {
-      return {
-        backgroundPosition: backgroundPosition,
-        transition: 'background-position 100s ease-out'
-      }
-    }
-
-    // Style for other states
-    return {
-      backgroundPosition: backgroundPosition,
-      transition: 'background-position 1s ease-out'
-    }
-  }
-
-  const backgroundPosition =
-    characterState === 'panic'
-      ? '0px -1680px'
-      : tapCount > 0
-      ? `${tapCount * -11}px ${tapCount * 0.085}px`
-      : '0 0'
-
+const Character = ({
+  tapCount,
+  setTapCount,
+  handleTap,
+  getCharacterGif,
+  characterStyles
+}) => {
   return (
     <div className='flex justify-center flex-col gap-4 items-center my-20 mx-8'>
-      <div
-        className={`bg-planet w-80 rounded-full ${
-          tapCount < 30 ? 'cursor-pointer' : 'cursor-not-allowed'
-        }`}
-        onClick={handleTap}
-        style={getBackgroundStyle()}
-      >
+      <div>
         <img
+          onClick={handleTap}
           src={getCharacterGif()}
           alt='character'
           className='chara w-80 h-80 rounded-full object-contain'
