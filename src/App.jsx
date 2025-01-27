0cd6b50c5d2734dad476d24291a282827a508857
Zakee ul Hassan <zakki5shah@gmail.com>
2025-01-27 00:39:23 -0300

    Loading page adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 5000);
+    }, 4000);
 
     return () => clearTimeout(timer); 
   }, []);
