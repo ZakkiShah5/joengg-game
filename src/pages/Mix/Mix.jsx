ea872f02d6560c9d6fc992d6f2c6aaf0576110b6
Zakee ul Hassan <zakki5shah@gmail.com>
2025-01-30 03:11:33 -0300

    changes made v01

--- src/pages/Mix/Mix.jsx
+++ src/pages/Mix/Mix.jsx
@@ -1,41 +1,46 @@
+import { useRef, useState } from 'react'
 import Back from './components/Back'
 import Speaker from '../Ranking/components/Speaker'
 import RateBoard from './components/RateBoard'
 import MixUp from './components/MixUp'
-import { useState } from 'react'
 
-import mixSound from '../../assets/sounds/3.coalescence.ogg'
-import mixSuccessSound from '../../assets/sounds/5.success.ogg'
-import mixFailSound from '../../assets/sounds/4.fail.wav'
+import mixSoundFile from '../../assets/sounds/3.coalescence.ogg'
+import mixSuccessSoundFile from '../../assets/sounds/5.success.ogg'
+import mixFailSoundFile from '../../assets/sounds/4.fail.wav'
 
 const Mix = () => {
-  const [mixSuccess, setMixSuccess] = useState(null) // To track if mix is successful or failed
+  const [mixSuccess, setMixSuccess] = useState(null)
   const [isMixing, setIsMixing] = useState(false)
+
+  const mixSound = useRef(new Audio(mixSoundFile))
+  const mixSuccessSound = useRef(new Audio(mixSuccessSoundFile))
+  const mixFailSound = useRef(new Audio(mixFailSoundFile))
+
+  const playSound = (audioRef) => {
+    audioRef.current.currentTime = 0 // Reset sound position
+    audioRef.current.play().catch((err) => console.error('Audio play error:', err))
+  }
 
   const handleBack = () => {
     setIsMixing(false)
   }
+
   const handleMix = () => {
-    const audio = new Audio(mixSound)
-    audio.load()
-    audio.play()
+    setIsMixing(true)
+    playSound(mixSound)
+    
     const success = Math.random() > 0.5 // 50% chance
-    console.log(success)
+    setMixSuccess(success)
+
     if (success) {
-      setIsMixing(true) // Proceed with mixing animations
-      setMixSuccess(true) // Mark the mix as successful
-      const audio = new Audio(mixSuccessSound)
-      audio.load()
-      audio.play()
-    }
-    if (!success) {
-      setIsMixing(true)
-      setMixSuccess(false)
-      const audio = new Audio(mixFailSound)
-      audio.load()
-      audio.play()
+      mixSuccessSound.current.onended = () => setIsMixing(false)
+      playSound(mixSuccessSound)
+    } else {
+      mixFailSound.current.onended = () => setIsMixing(false)
+      playSound(mixFailSound)
     }
   }
+
   return (
     <div className='bg-main'>
       <Back />
